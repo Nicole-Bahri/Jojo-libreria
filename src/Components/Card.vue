f345ad2281599e3c30b8fe4a8e6f4e42d06b9c2e
Santiago Martinez <peladio98@gmail.com>
2024-06-29 12:00:07 -0400

    error en la main

--- src/Components/Card.vue
+++ src/Components/Card.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="flex flex-col overflow-hidden rounded-xl shadow-xl">
-      <div>
-        <img v-if="props.character" :src="`https://jojos-bizarre-api.netlify.app/assets/${props.character.image}`" alt="image" width="200" height="350"/>
-      </div>
-      <div>
-        <h3>{{ props.character.name }}</h3>
-      </div>
+  <div class="flex flex-col overflow-hidden rounded-xl shadow-xl">
+    <div>
+      <img
+        v-if="props.character"
+        :src="`https://jojos-bizarre-api.netlify.app/assets/${props.character.image}`"
+        alt="image"
+        width="200"
+        height="350"
+      />
     </div>
+    <div>
+      <h3>{{ props.character.name }}</h3>
+    </div>
+  </div>
 </template>
 
 <script setup>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import Card from '@/Components/Card.vue';
-import router from '@/router'
+import Card from '@/Components/Card.vue'
 
 
 let character = ref([])
@@ -10,39 +9,35 @@ let page = ref({
   end: 10
 })
 
-const actualCharacters= ref([])
+const actualCharacters = ref([])
 
 async function loadCharacters() {
-  const response = await fetch(`https://stand-by-me.herokuapp.com/api/v1/characters?page=${page.value}`);
+  const response = await fetch(
+    `https://stand-by-me.herokuapp.com/api/v1/characters?page=${page.value}`
+  )
   const data = await response.json()
   character.value = data
-  
 }
 
-const changePage = ()=>{
-  
-  actualCharacters .value = character.value.slice(page.value.start, page.value.end)
+const changePage = () => {
+  actualCharacters.value = character.value.slice(page.value.start, page.value.end)
   console.log(actualCharacters)
-  page.value.start = page.value.start +10
-  page.value.end = page.value.end +10
+  page.value.start = page.value.start + 10
+  page.value.end = page.value.end + 10
 
-  console.log("start",page.value.start)
-  console.log("end",page.value.end)
-  
+  console.log('start', page.value.start)
+  console.log('end', page.value.end)
 }
 
 const previousPage = () => {
-
-  actualCharacters .value = character.value.slice(page.value.start, page.value.end)
+  actualCharacters.value = character.value.slice(page.value.start, page.value.end)
   console.log(actualCharacters)
-  page.value.start = page.value.start -10
-  page.value.end = page.value.end -10
+  page.value.start = page.value.start - 10
+  page.value.end = page.value.end - 10
 
-  console.log("start",page.value.start)
-  console.log("end",page.value.end)
-  
+  console.log('start', page.value.start)
+  console.log('end', page.value.end)
 }
-
 
 loadCharacters()
 </script>
@@ -52,15 +47,18 @@ loadCharacters()
     <h1 class="text-3xl md:text-5xl">Personajes de Jojo's</h1>
   </div>
 
-
   <div class="Y7eBOXZBuWX_SpAA0uHS" bis_skin_checked="1">
     <button @click="previousPage">-</button>
     <p>Si quieres volver, reinicia la pagina</p>
     <button @click="changePage">+</button>
   </div>
   <div class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 m-6">
-    <router-link v-for="personajes in actualCharacters" :key="personajes.id" :to="`/${personajes.id}`">
+    <router-link
+      v-for="personajes in actualCharacters"
+      :key="personajes.id"
+      :to="`/${personajes.id}`"
+    >
       <Card :character="personajes"></Card>
     </router-link>
   </div>
-</template> 
+</template>
