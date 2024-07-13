61bb4e0bee4aede55e593ccd5f958096e8658884
Nicole Bahri <bahrinicole1@gmail.com>
2024-07-13 09:38:46 -0400

    Arreglando el error :)

--- src/Components/Card.vue
+++ src/Components/Card.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="flex flex-col overflow-hidden rounded-xl shadow-xl">
-      <div>
-        <img :src="character.image" alt="" />
-      </div>
-      <div>
-        <h3>{{ props.character.name }}</h3>
-      </div>
+  <div class="flex flex-col overflow-hidden rounded-xl shadow-xl">
+    <div>
+      <img
+        v-if="props.character"
+        :src="`https://jojos-bizarre-api.netlify.app/assets/${props.character.image}`"
+        alt="image"
+        width="200"
+        height="350"
+      />
     </div>
-  </template>
+    <div>
+      <h3>{{ props.character.name }}</h3>
+    </div>
+  </div>
+</template>
 
 <script setup>
 const props = defineProps({
@@ -16,7 +22,4 @@ const props = defineProps({
     type: Object
   }
 })
-
-console.log(props.character)
-
 </script>
